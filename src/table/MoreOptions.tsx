ce970257ec8fdab50d311ad9e69b7ddcb33da66e
Tamir <1tamir198@gmail.com>
2024-08-21 10:00:51 +0300

    Refactor - render dynamyc modal content on select

--- src/table/MoreOptions.tsx
+++ src/table/MoreOptions.tsx
@@ -2,24 +2,58 @@ import { useState, MouseEvent } from "react";
 import { TableCell, Menu, MenuItem, IconButton } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { FC } from "react";
-import { ExportToExcel } from "./tableModals/ExportToExcel";
+import GenericModal from "../modal/GenericModal";
+
+export enum ModalOption {
+  EXPORT_TO_CSV,
+  EXPORT_TO_EXCEL,
+  OPTION_3,
+}
+
 export const MoreOptions: FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<ModalOption | null>(
+    null
+  );
 
   const handleClick = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleCloseMenu = () => {
     setAnchorEl(null);
   };
 
-  const handleOptionClick = (option: string) => {
-    if (option === "Option 1") {
-      setIsModalOpen(true);
+  const handleOptionClick = (option: ModalOption) => {
+    setSelectedOption(option);
+    handleCloseMenu();
+  };
+
+  const handleCloseModal = () => {
+    setSelectedOption(null);
+  };
+
+  const renderModalContent = () => {
+    switch (selectedOption) {
+      case ModalOption.EXPORT_TO_CSV:
+        return (
+          <>
+            <h1>Export your data into CSV</h1>
+            <h2>It's the new cool kid on the block</h2>
+          </>
+        );
+      case ModalOption.EXPORT_TO_EXCEL:
+        return (
+          <>
+            <h1>Export your data into Excel</h1>
+            <h2>Do it, it's very nice</h2>
+          </>
+        );
+      case ModalOption.OPTION_3:
+        return <h1>TODO fill out the missing parts</h1>;
+      default:
+        return <></>;
     }
-    handleClose();
   };
 
   return (
@@ -27,19 +61,29 @@ export const MoreOptions: FC = () => {
       <IconButton onClick={handleClick}>
         <MoreVertIcon />
       </IconButton>
-      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-        <MenuItem onClick={() => handleOptionClick("Option 1")}>
-          Option 1
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleCloseMenu}
+      >
+        <MenuItem onClick={() => handleOptionClick(ModalOption.EXPORT_TO_CSV)}>
+          Export to CSV
         </MenuItem>
-        <MenuItem onClick={() => handleOptionClick("Option 2")}>
-          Option 2
+        <MenuItem
+          onClick={() => handleOptionClick(ModalOption.EXPORT_TO_EXCEL)}
+        >
+          Export to Excel
         </MenuItem>
-        <MenuItem onClick={() => handleOptionClick("Option 3")}>
+        <MenuItem onClick={() => handleOptionClick(ModalOption.OPTION_3)}>
           Option 3
         </MenuItem>
       </Menu>
 
-      {isModalOpen && <ExportToExcel onClose={() => setIsModalOpen(false)} />}
+      {selectedOption !== null && (
+        <GenericModal onClose={handleCloseModal} open={true}>
+          {renderModalContent()}
+        </GenericModal>
+      )}
     </TableCell>
   );
 };
