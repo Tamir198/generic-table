501f0ddfdd713df4047dca89d698fbe12eecf736
Tamir <1tamir198@gmail.com>
2024-08-22 09:52:09 +0300

    Adding shadow to the tooltip ractangle

--- src/tooltip/GenericTooltip.tsx
+++ src/tooltip/GenericTooltip.tsx
@@ -26,12 +26,24 @@ export interface GenericTooltipProps extends TooltipProps {
   placement?: "top" | "bottom" | "right" | "left";
 }
 
-const StyledTooltip = styled(Tooltip)({});
+const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.text.primary,
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "white",
+    fontSize: 11,
+  },
+}));
 
 const StyledTooltipContent = styled(Box)({
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  background: "white",
 });
