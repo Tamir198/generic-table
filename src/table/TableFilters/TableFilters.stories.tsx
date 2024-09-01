13ac73286ae927ca682b941f351a652ff80e00d1
Tamir Abutbul <1tamir198@gmail.com>
2024-09-01 13:42:08 +0300

    Update table filters story

--- src/table/TableFilters/TableFilters.stories.tsx
+++ src/table/TableFilters/TableFilters.stories.tsx
@@ -1,6 +1,7 @@
 // src/stories/TableFilters.stories.tsx
 import { Meta, StoryObj } from '@storybook/react';
 import { TableFilters } from './TableFilters';
+import { OptionValue, MultiOptionValue } from '../../types';
 
 const meta: Meta<typeof TableFilters> = {
   title: 'Components/TableFilters',
@@ -12,14 +13,32 @@ const meta: Meta<typeof TableFilters> = {
     },
   },
   tags: ['autodocs'],
-  argTypes: {},
 } satisfies Meta<typeof TableFilters>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const mockOnBailStatusChange = (value: OptionValue) =>
+  console.log('Bail Status Changed:', value);
+const mockOnBailTypeChange = (value: MultiOptionValue) =>
+  console.log('Bail Type Changed:', value);
+const mockOnCoinTypeChange = (value: MultiOptionValue) =>
+  console.log('Coin Type Changed:', value);
+const mockClearFilters = () => console.log('Filters Cleared');
+const mockColumnTypes = {
+  name: 'string',
+  age: 'number',
+  dateOfBirth: 'date',
+};
+
 export const Default: Story = {
   render: (args) => <TableFilters {...args} />,
-  args: {},
+  args: {
+    onBailStatusChange: mockOnBailStatusChange,
+    onBailTypeChange: mockOnBailTypeChange,
+    onCoinTypeChange: mockOnCoinTypeChange,
+    clearFilters: mockClearFilters,
+    columnTypes: mockColumnTypes,
+  },
 };
